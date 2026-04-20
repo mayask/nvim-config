ea53b708fd047d3406c5d727c347d70700941295
Maxim Yaskevich <my@dahts.com>
2026-04-20 14:36:34 +0200

    sync config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,16 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- Absolute line numbers
+vim.opt.relativenumber = false
+vim.opt.number = true
+
+-- 4 space indentation
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Auto-reload files changed outside of Neovim
+vim.opt.autoread = true
+
+-- Trigger autoread when focus returns or buffer is entered
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+  command = "checktime",
+})
